476ba70bfb908eebedcec7ae95cb9dac6d89a728
lifuzhi <512116138@qq.com>
2016-09-02 09:53:18 +0800

    添加微信

--- collectiveWeiXin/controllers/CallableController.php
+++ collectiveWeiXin/controllers/CallableController.php
@@ -4,41 +4,27 @@ namespace app\collectiveWeiXin\controllers;
 use Yii;
 class CallableController extends \yii\web\Controller
 {
-    	public function actionIndex()
-    	{
-    		$request = Yii::$app->request;
+	public function actionIndex()
+	{
 
+		$request = Yii::$app->request;
 		$get = $request->get();
 
-	    	$signature = $request->get("signature",1);
-
-	        	$timestamp = $request->get("timestamp",1) ;
-
-	        	$nonce = $request->get("nonce",1) ;
-
-		$token = 'pgk123';
-
-		$tmpArr = array($token, $timestamp, $nonce);
-
-		sort($tmpArr, SORT_STRING);
-
-		$tmpStr = implode( $tmpArr );
-
-		$tmpStr = sha1( $tmpStr );
-
-
-		if( $tmpStr == $signature ){
-			Yii::trace('验证成功');
-			echo $request->get("echostr",1);
-			die;
-			// return true;
-
-		}else{
-			Yii::trace('验证失败');
-			return false;
-
+		//验证回调域名
+		if (!empty($get['signature']) && !empty($get['timestamp']) && !empty($get['nonce']) && !empty($get['echostr']) ) {
+			
+			$tmpArr = array(Yii::$app->params['collectiveWeixinConfig']['token'], $get['timestamp'], $get['nonce']);
+			sort($tmpArr, SORT_STRING);
+			$tmpStr = implode( $tmpArr );
+			$tmpStr = sha1( $tmpStr );
+			if( $tmpStr == $get['signature'] ){
+				echo $get['echostr'];
+				die;
+			}else{
+				return false;
+			}
 		}
-        		// return $this->render('index');
-    	}
+
+	}
 
 }
