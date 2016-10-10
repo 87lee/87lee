9708e32e1b23b93ec072307e3aea175ebbfe6707
style-lee <style-lee>
2016-10-11 00:04:31 +0800

    LOG

--- collectiveWeiXin/weixin.php
+++ collectiveWeiXin/weixin.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace app\collectiveWeiXin;
-
 /**
  * collectiveWeiXin module definition class
  */
@@ -11,13 +9,34 @@ class weixin extends \yii\base\Module
      * @inheritdoc
      */
     public $controllerNamespace = 'app\collectiveWeiXin\controllers';
+    
+    
+    protected function getAccessToken()
+    {
+        $params = $this->params;
+        Yii::trace('请求获取微信access_token');
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $params['collectiveWeixinConfig']['appId'] . '&secret='.$params['collectiveWeixinConfig']['appsecret'];
+        $str = \app\helpers\Url::getUrl($url);
+        if ($str === false ){
+            die('远程获取access_token失败');
+        }else{
+            $res = \yii\helpers\Json::decode($str);
+            if (isset($res['access_token'])) {
+                return $res;
+            }else{
+                die('微信端请求:'.$res['errcode'] .' 信息：'.$res['errmsg']);
+            }
+        }
+    }
     /**
      * 
      * @inheritdoc
      * style.com/collectiveWeiXin/callable
      */
+    
     public function init()
     {
+        Yii::trace('微信初始化');
         //继承父级初始化
         parent::init();
         //加入模块配置
@@ -30,6 +49,7 @@ class weixin extends \yii\base\Module
                 $accessTokenStr = file_get_contents($weixinAccessTokenFile);
                 $accessTokenArr = \yii\helpers\Json::decode($accessTokenStr);
             }
+            Yii::trace('验证微信access_token');
             if (!empty($accessTokenArr)) {
                 if ( $accessTokenArr['time'] + $accessTokenArr['expires_in'] - time() - 900 <= 0) {
                     $res = $this->getAccessToken();
@@ -44,24 +64,7 @@ class weixin extends \yii\base\Module
             file_put_contents($weixinAccessTokenFile, \yii\helpers\Json::encode($res));
 
             \Yii::$app->params['collectiveWeixinConfig'] = array_merge($this->params['collectiveWeixinConfig'],['access_token'=>$res['access_token']]);
-            
+
         });
-    }
-    
-    protected function getAccessToken()
-    {
-        $params = $this->params;
-        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $params['collectiveWeixinConfig']['appId'] . '&secret='.$params['collectiveWeixinConfig']['appsecret'];
-        $str = \app\helpers\Url::getUrl($url);
-        if ($str === false ){
-            die('远程获取access_token失败');
-        }else{
-            $res = \yii\helpers\Json::decode($str);
-            if (isset($res['access_token'])) {
-                return $res;
-            }else{
-                die('微信端请求:'.$res['errcode'] .' 信息：'.$res['errmsg']);
-            }
-        }
     }
 }
